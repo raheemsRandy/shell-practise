b22b40fdc056a4305e9131e58441b93ed5337073
raheemshaik <raheemshaik2404@gmail.com>
2025-06-22 18:03:13 +0000

    hello world

--- variables.sh
+++ variables.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 
-p1=Trump 
-p2=Musk
+# p1=Trump 
+# p2=Musk
+p1=$1
+p2=$2
 echo "$p1 : wat r u doing"
 echo "$p2 : iam building rocket to mars"
 echo "$p2 : And We are planning $p1 company"
