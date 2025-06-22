adc0cd0a8a41b843b2e82e5acd862f4bd48c72f3
raheemshaik <raheemshaik2404@gmail.com>
2025-06-22 20:27:14 +0000

    commit

--- conditionalinstall.sh
+++ conditionalinstall.sh
@@ -15,19 +15,31 @@ dnf list installed mysql
 if [ $? -ne 0 ]
 then    
     echo "mysql not installed going to install"
+
+    #dnf install wantederror mysql -y
+    dnf install mysql -y
+
+     if [ $? -eq 0 ]
+     then 
+        echo "Installing mysql .....Success"
+     else
+        echo "Installing mysql .....Failure"
+        exit 1
+fi
+
 else
     echo "mysql already installed nothing to do"
 fi
 
 
 
-#dnf install wantederror mysql -y
-dnf install mysql -y
+# #dnf install wantederror mysql -y
+# dnf install mysql -y
 
-if [ $? -eq 0 ]
-then 
-    echo "Installing mysql .....Success"
-else
-    echo "Installing mysql .....Failure"
-    exit 1
-fi
+# if [ $? -eq 0 ]
+# then 
+#     echo "Installing mysql .....Success"
+# else
+#     echo "Installing mysql .....Failure"
+#     exit 1
+# fi
