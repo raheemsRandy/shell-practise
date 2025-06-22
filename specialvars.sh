4abd879c48d71d5cbdebd09374bfb8c843f90af5
raheemshaik <raheemshaik2404@gmail.com>
2025-06-22 19:11:13 +0000

    commit

--- specialvars.sh
+++ specialvars.sh
@@ -1,1 +1,9 @@
-echo -e "all vars passed :$@\n no of vars :$#"
+echo -e "all vars passed :$@\n 
+no of vars :$#\n
+script name :$0\n
+current dir :$PWD\n 
+user : $USER\n
+home dir : $HOME\n
+pid of script :$$ \n
+`sleep 10 &`\n 
+pid og last bg cmd :$!"
