41d4bdc46cd85e73ca3e29ab6be6a5613c9e0f53
raheemshaik <raheemshaik2404@gmail.com>
2025-06-22 20:50:30 +0000

    commit

---
+++ functions.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+
+userId=$(id -u)
+
+if [ $userId -ne 0 ]
+then 
+    echo "please run this command with root access"
+    exit 1
+else
+    echo "Your are running with root access"
+fi
+Validate(){
+    if [ $1 -eq 0 ]
+     then 
+        echo "Installing $2 .....Success"
+     else
+        echo "Installing $2 .....Failure"
+        exit 1
+    fi
+}
+
+#--------------------------------------------------
+dnf list installed mysql
+if [ $? -ne 0 ]
+then    
+    echo "mysql not installed going to install"
+
+    #dnf install wantederror mysql -y
+    dnf install mysql -y
+    Validate $? mysql
+else
+    echo "mysql already installed nothing to do"
+fi
+
+#----------------------------------------------
+
+dnf list installed python
+if [ $? -ne 0 ]
+then    
+    echo "python not installed going to install"
+
+    #dnf install wantederror mysql -y
+    dnf install python -y
+    Validate $? python
+
+else
+    echo "python already installed nothing to do"
+fi
+
+#-----------------------------------------------------------
+
+dnf list installed nginx
+if [ $? -ne 0 ]
+then    
+    echo "nginx not installed going to install"
+
+    #dnf install wantederror mysql -y
+    dnf install nginx -y
+    Validate $? nginx
+
+else
+    echo "nginx already installed nothing to do"
+fi
+
+
+# #dnf install wantederror mysql -y
+# dnf install mysql -y
+
+# if [ $? -eq 0 ]
+# then 
+#     echo "Installing mysql .....Success"
+# else
+#     echo "Installing mysql .....Failure"
+#     exit 1
+# fi
