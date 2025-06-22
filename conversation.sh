83c30f511ef54e1196227947240c9530c0068f08
raheemshaik <raheemshaik2404@gmail.com>
2025-06-22 17:45:21 +0000

    hello world

--- conversation.sh
+++ conversation.sh
@@ -2,8 +2,8 @@
 
 
 p1 = "Trump"  
-p2 = "Musk""
-echo "$p1 : wat r u doing "
+p2 = "Musk"
+echo "$p1 : wat r u doing"
 echo "$p2 : iam building rocket to mars"
 echo "$p2 : And We are planning $p1 and $p2 company"
 
