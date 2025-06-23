d60f65c80539e9a53665a465aedca1cd96560126
raheemshaik <raheemshaik2404@gmail.com>
2025-06-23 18:54:41 +0530

    commit

--- loops.sh
+++ loops.sh
@@ -45,7 +45,7 @@ Validate(){
 #------------------------------------------------------
 
 # for i in ${Packages[@]}
-for i in {$@}
+for i in $@
 do
     dnf list installed $i&>>$Log_file
     if [ $? -ne 0 ]
